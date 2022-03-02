bc7c2bf15f49a9848927c000902aeedfd6b1683e
Vantol Bennett <vvantol2000@icloud.com>
2022-03-01 20:08:35 -0500

    added authRepo

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,13 @@ import { UsersModule } from './users/users.module';
 import { SermonModule } from './sermon/sermon.module';
 import { PrayerModule } from './prayer/prayer.module';
 import { BibleStudyModule } from './bible-study/bible-study.module';
+import { User } from './auth/entities/users.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [User],
     synchronize: true,
   }), AuthModule, UsersModule, SermonModule, PrayerModule, BibleStudyModule],
   controllers: [AppController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/users.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
+    
+}
