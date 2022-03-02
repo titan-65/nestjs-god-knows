7dc883065bcefba48821cca696a92f2504dd8d9a
Vantol Bennett <vvantol2000@icloud.com>
2022-03-01 21:20:20 -0500

    CreateDto created

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
