2c29756e3ea34996062a8e31fd3025d79f833c35
Vantol Bennett <vvantol2000@icloud.com>
2022-03-06 13:44:31 -0500

    added UserModule to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Auth } from './entities/auth.entity';
+import { UsersModule } from '../users/users.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auth])],
+  imports: [TypeOrmModule.forFeature([Auth]), UsersModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
