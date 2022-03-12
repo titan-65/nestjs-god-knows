d8e8bae4ec4529be0939bf60f73d97f9f18923ce
Vantol Bennett <vvantol2000@icloud.com>
2022-03-12 00:09:22 -0500

    modified auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,26 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Auth } from './entities/auth.entity';
 import { UsersModule } from '../users/users.module';
-
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
+import { jwtConstants} from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auth]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Auth]),
+    UsersModule,
+    PassportModule.register({
+    defaultStrategy: 'jwt',
+    property: 'user',
+    session: false,
+  }), JwtModule.register({
+      // TODO: Add environment variables
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [PassportModule, JwtModule],
 })
 export class AuthModule {}
